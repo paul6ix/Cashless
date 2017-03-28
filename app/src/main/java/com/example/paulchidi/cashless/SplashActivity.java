537f36cc050d901c2a4a2f26a517a5089d86415d
Paul6ix <okporp@gmail.com>
2017-03-28 14:56:22 +0100

    first build

---
+++ app/src/main/java/com/example/paulchidi/cashless/SplashActivity.java
@@ -0,0 +1,43 @@
+package com.example.paulchidi.cashless;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread splash = new Thread(){
+            public void run() {
+                try {
+                    sleep(3*1000);
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+
+
+                }
+                catch (Exception e){
+                    //TODO
+                }
+            }
+        };
+        splash.start();
+
+
+    }
+}
