642c57d816c7d38e6513891dcdab2a0bad22491f
Paul6ix <okporp@gmail.com>
2017-04-07 23:52:58 +0100

    Refactored reader activity

---
+++ app/src/main/java/com/example/paulchidi/cashless/activities/ReaderActivity.java
@@ -0,0 +1,46 @@
+package com.example.paulchidi.cashless.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.example.paulchidi.cashless.R;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class ReaderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reader);
+        //Adding Barcode Code
+        final Activity activity = this;
+        IntentIntegrator integrator = new IntentIntegrator(activity);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+        integrator.setPrompt("Scan");
+        integrator.setCameraId(0);
+        integrator.setBeepEnabled(false);
+        integrator.setBarcodeImageEnabled(false);
+        integrator.initiateScan();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "You cancelled the Scanning", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+
+            }
+        } else {
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
--- app/src/main/java/com/example/paulchidi/cashless/fragments/HomeFragment.java
+++ app/src/main/java/com/example/paulchidi/cashless/fragments/HomeFragment.java
@@ -1,13 +1,16 @@
 package com.example.paulchidi.cashless.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.paulchidi.cashless.R;
+import com.example.paulchidi.cashless.activities.ReaderActivity;
 
 
 public class HomeFragment extends Fragment {
@@ -17,7 +20,19 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        ImageButton scanQr = (ImageButton) v.findViewById(R.id.imageButton_ScanQr);
+        scanQr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ReaderActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return v;
+
     }
 
 }
