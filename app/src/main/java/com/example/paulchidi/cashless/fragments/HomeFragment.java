29e4aaf4691891ccd7ceb1b0e045f60d970b9363
Paul6ix <okporp@gmail.com>
2017-04-08 00:14:50 +0100

    Fixed Bug

--- app/src/main/java/com/example/paulchidi/cashless/fragments/HomeFragment.java
+++ app/src/main/java/com/example/paulchidi/cashless/fragments/HomeFragment.java
@@ -1,23 +1,37 @@
 package com.example.paulchidi.cashless.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.paulchidi.cashless.R;
+import com.example.paulchidi.cashless.activities.ReaderActivity;
 
 
 public class HomeFragment extends Fragment {
+
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        ImageButton ScanQrButton = (ImageButton) v.findViewById(R.id.imageButton_ScanQr);
+        ScanQrButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ReaderActivity.class);
+                startActivity(intent);
+            }
+        });
+        return v;
     }
+
 
 }
