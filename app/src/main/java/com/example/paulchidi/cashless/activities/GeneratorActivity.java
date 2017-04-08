028ad15ea8a56ac81cc9d18665e4ddf29f5307c9
Paul6ix <okporp@gmail.com>
2017-04-08 05:06:41 +0100

    customized display text for reader

--- app/src/main/java/com/example/paulchidi/cashless/activities/GeneratorActivity.java
+++ app/src/main/java/com/example/paulchidi/cashless/activities/GeneratorActivity.java
@@ -1,9 +1,13 @@
 package com.example.paulchidi.cashless.activities;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -17,31 +21,63 @@ import com.journeyapps.barcodescanner.BarcodeEncoder;
 public class GeneratorActivity extends AppCompatActivity {
     ImageView imageView_QrCode;
     Button button_QrCode;
-    EditText editText_QrData;
-    String text2QR;
+    EditText editText_QrDataName;
+    EditText editText_QrDataAcctNo;
+    EditText editText_QrDataAmount;
+    String qrData;
+    String qrMessage;
+    String acct;
+    String name;
+    String amount;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generator);
         imageView_QrCode = (ImageView) findViewById(R.id.imageView_QrCode);
-        editText_QrData = (EditText) findViewById(R.id.editText_generate);
+        editText_QrDataName = (EditText) findViewById(R.id.editText_accountname);
+        editText_QrDataAcctNo = (EditText) findViewById(R.id.editText_accountnum);
+        editText_QrDataAmount = (EditText) findViewById(R.id.editText_amount);
         button_QrCode = (Button) findViewById(R.id.button_generate);
         button_QrCode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                MultiFormatWriter formatWriter = new MultiFormatWriter();
-                text2QR = editText_QrData.getText().toString();
-                try {
-                    BitMatrix bitMatrix = formatWriter.encode(text2QR, BarcodeFormat.QR_CODE, 200, 200);
-                    BarcodeEncoder encoder = new BarcodeEncoder();
-                    Bitmap bitmap = encoder.createBitmap(bitMatrix);
-                    imageView_QrCode.setImageBitmap(bitmap);
-
-
-                } catch (Exception e) {
-                    e.printStackTrace();
+                amount = editText_QrDataAmount.getText().toString();
+                name = editText_QrDataName.getText().toString();
+                acct = editText_QrDataAcctNo.getText().toString();
+                //to hide keyboard
+                if (view != null) {
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
                 }
+                //end hide keyboard code
+                qrMessage = "Do you wish to transfer #" + amount + " to " + name.toUpperCase() + " with account Number: " + acct;
+                AlertDialog.Builder dialog = new AlertDialog.Builder(GeneratorActivity.this);
+                dialog.setTitle("Confirmation");
+                dialog.setMessage(qrMessage);
+                dialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        MultiFormatWriter formatWriter = new MultiFormatWriter();
+                        qrData = "Account Name: " + name + "\nAccount No: " + acct + "\nAmount: " + amount;
+                        try {
+                            BitMatrix bitMatrix = formatWriter.encode(qrData, BarcodeFormat.QR_CODE, 200, 200);
+                            BarcodeEncoder encoder = new BarcodeEncoder();
+                            Bitmap bitmap = encoder.createBitmap(bitMatrix);
+                            imageView_QrCode.setImageBitmap(bitmap);
+
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                });
+                dialog.setNegativeButton("Cancel", null);
+                dialog.show();
+
 
             }
         });
--- app/src/main/java/com/example/paulchidi/cashless/activities/ReaderActivity.java
+++ app/src/main/java/com/example/paulchidi/cashless/activities/ReaderActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.paulchidi.cashless.R;
@@ -11,11 +12,15 @@ import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 public class ReaderActivity extends AppCompatActivity {
+    TextView textView_scanResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reader);
+        textView_scanResult = (TextView) findViewById(R.id.textView_scanDetails);
+
+
+
         //Adding Barcode Code
         final Activity activity = this;
         IntentIntegrator integrator = new IntentIntegrator(activity);
@@ -30,12 +35,15 @@ public class ReaderActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if (result != null) {
             if (result.getContents() == null) {
-                Toast.makeText(this, "You cancelled the Scanning", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Scan Not Successful", Toast.LENGTH_SHORT).show();
             } else {
                 Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+
 
             }
         } else {
